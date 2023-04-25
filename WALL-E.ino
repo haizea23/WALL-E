a6b6fc70178b41e554a2dbb70ceee37c268840ee
haizea23 <125015549+haizea23@users.noreply.github.com>
2023-04-25 09:58:40 +0200

    Add files via upload

---
+++ WALL-E.ino
@@ -0,0 +1,44 @@
+#include <Traktorinoa.h>
+#include<UltraDistSensor.h>
+#include <Servo.h>
+
+Traktorinoa niretraktorinoa;
+UltraDistSensor mysensor;
+Servo nireSerboa; // Serbo objetua sortzen du
+float reading;
+
+void setup()
+{
+  Serial.begin(9600);
+  mysensor.attach(7, 8); //Trigger pin , Echo pin
+nireSerboa.attach(9); // Serboa  Arduinoko 6. Pin-era
+nireSerboa.write(50);
+}
+
+void loop() {
+
+  if (Serial.available() > 0) {
+    char data = Serial.read(); // bluetooth modulutik datorren datua irakurri
+    switch (data)
+    {
+      case 'a': niretraktorinoa.aurrera(); break; 
+      case 'b': niretraktorinoa.atzera(); break; 
+      case 'c': niretraktorinoa.ezkerrera(); break; 
+      case 'd': niretraktorinoa.eskuinera(); break; 
+      case 's': nireSerboa.write(80); break;      // 50 open clap   80 closed clap
+      case 'i': nireSerboa.write(50); break; 
+      case 'p': niretraktorinoa.geratu(); break;  
+      default : break;
+    }
+  }
+  delay(50);
+ 
+ // reading = mysensor.distanceInCm();
+  while (reading >1 && reading <40){
+    niretraktorinoa.geratu();
+    nireSerboa.write(90);
+    delay (5000);
+    niretraktorinoa.atzera();
+    reading = mysensor.distanceInCm();
+    } 
+}
